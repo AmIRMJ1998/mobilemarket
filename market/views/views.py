dc7265948b0cf03de9a8162425d8ac4e1ca1eaf8
moein taheri <moientaheri.1998@gmail.com>
2022-01-22 20:03:24 +0330

    sfgdfhjgmh,j

--- market/views/views.py
+++ market/views/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render, redirect
-from django.shortcuts import get_object_or_404
-from django.contrib.auth.models import User
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate,login,logout
 from django.http import HttpResponseRedirect
 from django.http import JsonResponse
 from django.urls import reverse
